5ed6bf855639e5f5bb85770ed25787720bd5fbf3
andresito0001 <ac07161025@gmail.com>
2024-10-29 10:50:12 +0000

    JDBC connection

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@ import java.sql.*;
 
 public class Main {
     public static void main (String args[]) {
-        System.out.println("hola mundo");
-
+        try {
+            String url = "URL HERE";
+            String user = "USER HERE";
+            String pass = "PASS HERE";
+            
+            Connection conn = DriverManager.getConnection(url, user, pass);
+            System.out.println("Done!");
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getCause());
+        }
     }
 }
