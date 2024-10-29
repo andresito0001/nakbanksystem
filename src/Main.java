b25f2ef3430b9f81c62e36ca4c01b54a872ffb9d
andresito0001 <ac07161025@gmail.com>
2024-10-29 15:05:43 +0000

    authentication demo

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main (String args[]) {
         try {
-            String url = "URL HERE";
-            String user = "USER HERE";
-            String pass = "PASS HERE";
-            
+            Scanner sc = new Scanner(System.in);
+            String url, user, pass;
+            System.out.print("Welcome to NakBank System!\nEnter url: ");
+            url = sc.nextLine();
+            System.out.print("Enter username: ");
+            user = sc.nextLine();
+            System.out.print("Enter password: ");
+            pass = sc.nextLine();
+           
             Connection conn = DriverManager.getConnection(url, user, pass);
             System.out.println("Done!");
+            
+            Statement st = conn.createStatement();
+
+            ResultSet rs = st.executeQuery("select * from cliente");
+            
+            while (rs.next()) {
+                System.out.print("Column 1 returned ");
+                System.out.println(rs.getString(1));
+            }
+            
+            rs.close();
+            st.close();
             conn.close();
         } catch (SQLException e) {
             System.out.println(e.getCause());
