8a2e89a542000be8772cc1aab331b6faef157772
andresito0001 <70287752+andresito0001@users.noreply.github.com>
2024-11-04 16:36:16 -0400

    Update clientes.java

--- queries/Clientes.java
+++ queries/Clientes.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 import java.sql.ResultSet;
 
 public class Clientes {
@@ -79,6 +80,43 @@ public class Clientes {
             st.close();
         } catch (SQLException e) {
             System.err.println("[ERROR]: " + e.getSQLState());
+        }
+    }
+
+    public void reporteTopCliente(Connection conn) {
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Ingrese el mes para ver el top clientes (1 es enero, 12 es diciembre)");
+            String mes = sc.nextLine();
+
+            String calcularPromedio = "select avg (cantidad_recibida/cantidad_enviada) as promedio from transaccion where moneda_recibida = 'bs' and tipo = 'venta' " + 
+            " and extract (month from fecha) = " + mes + " ;";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(calcularPromedio);
+            double promedioTasa = 0;
+
+            while (rs.next()) {
+                promedioTasa = rs.getDouble("promedio");                
+            }
+
+            String consultaTopCliente = "select concat (nombre, ' ', apellido) as cliente, count(*) as cantidad_trx, " +
+            "(sum(cantidad_recibida) * avg(cantidad_enviada/cantidad_recibida))/" + promedioTasa + " as profit " +
+            " from transaccion inner join cliente on transaccion.cedula_cliente = cliente.cedula where moneda_enviada = 'bs' and tipo = 'compra' " + 
+            " and extract (month from fecha) = " + mes + " group by cliente.cedula order by profit desc";
+
+            System.out.println("Promedio de Tasa Bs en las transacciones: " + promedioTasa + "\nTOP CLIENTES: ");
+
+            rs = st.executeQuery(consultaTopCliente);
+            System.out.println(" | Cliente" + " | Cantidad Transacciones | Profit |");
+            while (rs.next()) {
+                System.out.println (" | " + rs.getString("cliente")+ " | "+rs.getString("cantidad_trx") + " | " + rs.getDouble("profit") + " | ");
+            }
+
+            rs.close();
+            st.close();
+        } catch (SQLException e) {
+            System.out.println("Error de conexion. " + e.getCause());
         }
     }
 }
