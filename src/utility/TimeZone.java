e6dc99fd160e841c655943615e460f4a84f0e0d9
andresito0001 <70287752+andresito0001@users.noreply.github.com>
2024-12-02 15:30:25 -0400

    Create TimeZone.java

---
+++ src/utility/TimeZone.java
@@ -0,0 +1,29 @@
+package utility;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeZone {
+    public static String getDateZoneCaracas() {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("America/Caracas"));
+        String formattedDateTime = zonedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        return formattedDateTime;
+    }
+
+    public static String getTimeZoneCaracas() {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("America/Caracas"));
+        String formattedTime = zonedDateTime.format(DateTimeFormatter.ofPattern("hh:mm:ss"));
+        return formattedTime;
+    }
+
+    public static String obtenerHoraCaracas() {
+        LocalTime nowInCaracas = LocalTime.now(ZoneId.of("America/Caracas"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String formattedTime = nowInCaracas.format(formatter);
+
+        return formattedTime.trim();
+    }
+
+}
