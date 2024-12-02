13df38c853ecec8a123e53bc5478416e8b417dbd
andresito0001 <70287752+andresito0001@users.noreply.github.com>
2024-12-02 15:30:07 -0400

    Create checkTransType.java

---
+++ src/utility/CheckTypes/checkTransType.java
@@ -0,0 +1,19 @@
+package utility.CheckTypes;
+
+import java.util.Map;
+
+import utility.TransTypes;
+
+public class checkTransType {
+    public static String check(final Character ch) {
+        if (ch == null) return null;
+            
+        Map<Character, String> transactionTypes = Map.of (
+            'C', TransTypes.COMPRA.getTransType(),
+            'V', TransTypes.VENTA.getTransType(),
+            'I', TransTypes.INVERSION.getTransType()
+        );
+
+        return transactionTypes.getOrDefault(ch, null);
+    }
+}
