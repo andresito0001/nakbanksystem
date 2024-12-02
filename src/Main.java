97f15317db1f0db923ca85ac3917ddf669fc9108
andresito0001 <70287752+andresito0001@users.noreply.github.com>
2024-12-02 15:29:01 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import queries.Clientes;
-// import java.util.Scanner;
+import queries.Transacciones;
 
 public class Main {
     public static void main (String args[]) {
@@ -18,12 +17,140 @@ public class Main {
             pass = sc.nextLine();
            */
             
-            final Connection conn = DriverManager.getConnection("url", "user", "pass");
+            final Connection conn = DriverManager.getConnection();
             System.out.println("Done!");
+                    
+    //   //  tabla simutrans
+    //        List<String> colums = List.of (
+    //         "referencia",
+    //             "cedula_cliente",
+    //             "admin",
+    //             "fecha",
+    //             "hora",
+    //             "tipo",
+    //             "cantidad_recibida",
+    //             "moneda_recibida",
+    //             "metodo_recibido",
+    //             "cantidad_enviada",
+    //             "moneda_enviada",
+    //             "metodo_enviado",
+    //             "status",
+    //             "tasa",
+    //             "ganancia",
+    //             "ref_bancaria"
+    //        );
+
+    //        List<String> types = List.of(
+    //             "serial primary key",
+    //             "text references clienteS(cedula)",
+    //             "text references administradores(nombre_usuario)",
+    //             "date default current_date",
+    //             "TIME WITH TIME ZONE DEFAULT timezone('America/Caracas', CURRENT_TIME)",
+    //             "text",
+    //             "double precision",
+    //             "text",
+    //             "text references bancos(codigo)",
+    //             "double precision",
+    //             "text",
+    //             "text references bancos(codigo)",
+    //             "text",
+    //             "double precision",
+    //             "double precision",
+    //             "text not null"
+    //        );
+
+        // tabla simuinv
+            // List<String> colums = List.of (
+            //     "referencia",
+            //     "fecha",
+            //      "hora",
+            //     "tipo_movimiento",
+            //     "cantidad",
+            //     "moneda",
+            //     "metodo",
+            //     "tipo"
+            // );
+
+            // List<String> types = List.of (
+            //     "serial primary key",
+            //     "date",
+            //     "time with time zone",
+            //     "text",
+            //     "double precision",
+            //     "text",
+            //     "text",
+            //     "text"
+            // );
             
-            conn.close();
+            // // BANCOS
+            // List<String> colums = List.of (
+            //     "id_cuenta",
+            //     "codigo",
+            //     "nombre_banco",
+            //     "numero_cuenta",
+            //     "moneda",
+            //     "saldo_actual"
+            // );
+
+            // List<String> types = List.of (
+            //     "serial primary key",
+            //     "text unique not null",
+            //     "text",
+            //     "text",
+            //     "text",
+            //     "double precision"
+            // );
+
+            // TABLA ADMINISTRADORES 
+            // List<String> colums = List.of(
+            //     "id_administrador",
+            //     "nombre_usuario",
+            //     "password",
+            //     "estado"
+            // );
+
+            // List<String> types = List.of(
+            //     "serial primary key",
+            //     "text unique not null",
+            //     "text not null",
+            //     "text"
+            // );
+
+
+
+            // TABLA CLIENTES
+            // List<String> colums = List.of(
+            //     "id_cliente",
+            //     "cedula",
+            //     "nombre",
+            //     "apellido",
+            //     "alias"
+            // );
+            
+            // List<String> types = List.of(
+            //     "serial",
+            //     "text primary key",
+            //     "text",
+            //     "text",
+            //     "text"
+            // );
+
+
+           Transacciones.newRegister(conn);
+            
+
+           // crea una nueva tabla
+            // Transacciones.createTable(conn, "simutrans", colums, types);
+
+            // elimina una tabla
+        //    String query = "drop table simutrans;";
+
+        //     try (final PreparedStatement st = conn.prepareStatement(query)) {
+        //         st.executeUpdate();
+        //         st.close();
+        //     }
         } catch (SQLException e) {
-            System.out.println(e.getCause());
+            System.out.println(e.getMessage());
         }
     }
 }
