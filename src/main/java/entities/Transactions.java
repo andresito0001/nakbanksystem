40c7ff4c670fa75f605b8ec9d6e9ff5f5a8da04a
andresito0001 <70287752+andresito0001@users.noreply.github.com>
2024-12-18 13:24:49 -0400

    Update Transactions.java

--- src/main/java/entities/Transactions.java
+++ src/main/java/entities/Transactions.java
@@ -2,42 +2,20 @@ package main.java.entities;
 
 import java.sql.Connection;
 
-public class Transactions {
-    Transactions(final Connection conn, final Clients client, final Admin admin, final String date, final String time, final String type,
+public class Transactions extends Cicle {
+    Transactions(final Connection conn, final String id, final String parent_id, final Clients client, final Admin admin, final String date, final String time, final String type,
     final Double quantityReceived, final String currencyReceived, final String receivedMethod,
     final Double sentQuantity, final String sentCurrency, final String sentMethod, final String status, 
     final Double rate, final Double revenue, final String bankRef) {
-
-        this.client = client;
-        this.admin = admin;
-        this.date = date;
-        this.time = time;
-        this.type = type;
-        this.quantityReceived = quantityReceived;
-        this.currencyReceived = currencyReceived;
-        this.receivedMethod = receivedMethod;
-        this.sentQuantity = sentQuantity;
-        this.sentCurrency = sentCurrency;
-        this.sentMethod = sentMethod;
-        this.status = status;
-        this.rate = rate;
+        super(conn, id, client, admin, date, time, quantityReceived,
+        currencyReceived, receivedMethod, sentQuantity, sentCurrency, sentMethod, status, rate, bankRef);
+        
+        this. parent_id = parent_id;
         this.revenue = revenue;
-        this.bankRef = bankRef;
+        this.type = type;
     }
     
-    private final Clients client;
-    private final Admin admin;
-    private final String date;
-    private final String time;
     private final String type;
-    private final Double quantityReceived;
-    private final String currencyReceived;
-    private final String receivedMethod;
-    private final Double sentQuantity;
-    private final String sentCurrency;
-    private final String sentMethod;
-    private final String status;
-    private final Double rate; 
+    private final String parent_id;
     private final Double revenue;
-    private final String bankRef;
 }
