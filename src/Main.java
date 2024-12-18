926eb581e8ef799bab6f5a202202fb42f7613f7b
andresito0001 <70287752+andresito0001@users.noreply.github.com>
2024-12-18 13:26:31 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import main.test.java.transactionsAppCLI;
+
+// number of id's 1210000000000000000000000
 
 public class Main {
     public static void main (String args[]) {
@@ -16,141 +19,11 @@ public class Main {
             pass = sc.nextLine();
            */
             
-            final Connection conn = DriverManager.getConnection();
-            conn.close();
-            
-            System.out.println("Done!");
+            final Connection conn = DriverManager.getConnection("", "", "");
 
+            transactionsAppCLI app = new transactionsAppCLI(conn);
+            app.executeApp();
 
-            
-    //   //  tabla simutrans
-    //        List<String> colums = List.of (
-    //         "referencia",
-    //             "cedula_cliente",
-    //             "admin",
-    //             "fecha",
-    //             "hora",
-    //             "tipo",
-    //             "cantidad_recibida",
-    //             "moneda_recibida",
-    //             "metodo_recibido",
-    //             "cantidad_enviada",
-    //             "moneda_enviada",
-    //             "metodo_enviado",
-    //             "status",
-    //             "tasa",
-    //             "ganancia",
-    //             "ref_bancaria"
-    //        );
-
-    //        List<String> types = List.of(
-    //             "serial primary key",
-    //             "text references clienteS(cedula)",
-    //             "text references administradores(nombre_usuario)",
-    //             "date default current_date",
-    //             "TIME WITH TIME ZONE DEFAULT timezone('America/Caracas', CURRENT_TIME)",
-    //             "text",
-    //             "double precision",
-    //             "text",
-    //             "text references bancos(codigo)",
-    //             "double precision",
-    //             "text",
-    //             "text references bancos(codigo)",
-    //             "text",
-    //             "double precision",
-    //             "double precision",
-    //             "text not null"
-    //        );
-
-        // tabla simuinv
-            // List<String> colums = List.of (
-            //     "referencia",
-            //     "fecha",
-            //      "hora",
-            //     "tipo_movimiento",
-            //     "cantidad",
-            //     "moneda",
-            //     "metodo",
-            //     "tipo"
-            // );
-
-            // List<String> types = List.of (
-            //     "serial primary key",
-            //     "date",
-            //     "time with time zone",
-            //     "text",
-            //     "double precision",
-            //     "text",
-            //     "text",
-            //     "text"
-            // );
-            
-            // // BANCOS
-            // List<String> colums = List.of (
-            //     "id_cuenta",
-            //     "codigo",
-            //     "nombre_banco",
-            //     "numero_cuenta",
-            //     "moneda",
-            //     "saldo_actual"
-            // );
-
-            // List<String> types = List.of (
-            //     "serial primary key",
-            //     "text unique not null",
-            //     "text",
-            //     "text",
-            //     "text",
-            //     "double precision"
-            // );
-
-            // TABLA ADMINISTRADORES 
-            // List<String> colums = List.of(
-            //     "id_administrador",
-            //     "nombre_usuario",
-            //     "password",
-            //     "estado"
-            // );
-
-            // List<String> types = List.of(
-            //     "serial primary key",
-            //     "text unique not null",
-            //     "text not null",
-            //     "text"
-            // );
-
-
-
-            // TABLA CLIENTES
-            // List<String> colums = List.of(
-            //     "id_cliente",
-            //     "cedula",
-            //     "nombre",
-            //     "apellido",
-            //     "alias"
-            // );
-            
-            // List<String> types = List.of(
-            //     "serial",
-            //     "text primary key",
-            //     "text",
-            //     "text",
-            //     "text"
-            // );
-
-
-            
-
-           // crea una nueva tabla
-            // Transacciones.createTable(conn, "simutrans", colums, types);
-
-            // elimina una tabla
-        //    String query = "drop table simutrans;";
-
-        //     try (final PreparedStatement st = conn.prepareStatement(query)) {
-        //         st.executeUpdate();
-        //         st.close();
-        //     }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
