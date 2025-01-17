4bbf2c544211cf9c4149b78c516103e382f2ec00
andresito0001 <70287752+andresito0001@users.noreply.github.com>
2025-01-17 12:09:03 -0400

    Create ConnectionPool.java

---
+++ src/main/java/util/ConnectionPool.java
@@ -0,0 +1,25 @@
+package main.java.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionPool {
+    private ConnectionPool() {}
+
+    public static final Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                String url = System.getenv("NAKBANK_DB_URL");
+                String user = System.getenv("NAKBANK_DB_USERNAME");
+                String password = System.getenv("NAKBANK_DB_PASSWORD");
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    private static Connection connection;
+}
