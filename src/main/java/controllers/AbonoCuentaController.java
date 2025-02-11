a8683fca18cdd68cecaefe81e80c94fbf34b01da
Feysmar <feysmar17@gmail.com>
2025-02-11 14:24:41 -0400

    fixed msg error in abono controller
    Arreglado el error al abonar cuenta por cobrar

--- src/main/java/controllers/AbonoCuentaController.java
+++ src/main/java/controllers/AbonoCuentaController.java
@@ -4,9 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -20,7 +18,6 @@ import javafx.scene.input.MouseEvent;
 import main.java.dao.BanksDAO;
 import main.java.dao.InventoryDAO;
 import main.java.entities.CxC;
-import main.java.entities.Inventory;
 import main.java.util.ConnectionPool;
 import main.java.util.DatabaseUtils;
 import main.java.util.SceneSwitcher;
@@ -54,10 +51,13 @@ public class AbonoCuentaController {
         public void initialize () throws SQLException {
             
 
-        String typeMoneyReceived = cuentaXAbonar.getMonedaTransaccion();
+        String typeMoneyReceived = cuentaXAbonar.getMonedaTransaccion(); 
         List<String> bankCodes = new ArrayList<>();
-        bankCodes = new BanksDAO(ConnectionPool.getConnection()).getInfoOf("codigo", "moneda", typeMoneyReceived);
-        
+        bankCodes = new BanksDAO(ConnectionPool.getConnection()).getInfoOf("codigo","moneda", typeMoneyReceived); //query = "select " + colum + " from bancos where " + key + " = " + "'" + USD' OR MONEDA = 'USDT + "'" + ";";
+        if (typeMoneyReceived.equals("USD"))
+            {
+                bankCodes = new BanksDAO(ConnectionPool.getConnection()).getInfoOf("codigo", "moneda", "USD' or moneda = 'USDT");
+            }
         metodoId.getItems().addAll(bankCodes); 
        
         montoMaxId.setText("Monto debe ser menor o igual a " + cuentaXAbonar.getPendienteTransaccion());
@@ -69,7 +69,7 @@ public class AbonoCuentaController {
                 String codeBank = metodoId.getSelectionModel().getSelectedItem();
                 DatabaseUtils dbUtils = new DatabaseUtils(ConnectionPool.getConnection());
                 String nameBank = dbUtils.getValueOf("nombre_banco", "bancos", "codigo = '" + codeBank + "'").toString();
-        
+                
                 bankInfoId.setText(codeBank + " " + nameBank);
             } catch (Exception ex) {
                 ex.printStackTrace();
@@ -97,15 +97,17 @@ public class AbonoCuentaController {
                 byte[] random = new byte[] { 0x1, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9 };
                 final String id_inventario = ULID.generate(System.currentTimeMillis(), random);
                 
-                String idAbono = "", tipoMovimiento = "", tipoMetodo = "";
+                String idAbono = "", tipoMovimiento = "", tipoMetodo = "", tipoMoneda = "";
                 BanksDAO banksDAO = new BanksDAO(ConnectionPool.getConnection());
                 final Double totalBalance = banksDAO.getTotalBalanceOf(metodoId.getSelectionModel().getSelectedItem());
+                final String moneda = dbUtils.getValueOf("moneda", "bancos", "codigo = '" + metodoId.getSelectionModel().getSelectedItem() + "'").toString();
                 Double balanceNuevo = 0.0;
                 
                 if (tipoCuenta == "CUENTASXCOBRAR") {
                     idAbono = "I-" + id_inventario;
                     tipoMovimiento = "INGRESO";
                     tipoMetodo = "metodo_recibido";
+                    tipoMoneda = "moneda_recibida";
                     balanceNuevo = totalBalance + monto;
 
                 }
@@ -113,19 +115,20 @@ public class AbonoCuentaController {
                     idAbono = "E-" + id_inventario;
                     tipoMovimiento = "EGRESO";
                     tipoMetodo = "metodo_enviado";
+                    tipoMoneda = "moneda_enviada";
                     balanceNuevo = totalBalance - monto;
                 }
 
-                if (totalBalance <= 0 || totalBalance < monto && tipoCuenta == "CUENTASXPAGAR")
+                if (totalBalance <= 0 && tipoCuenta == "CUENTASXPAGAR"|| totalBalance < monto && tipoCuenta == "CUENTASXPAGAR")
                 {
                     Alert alert = new Alert(AlertType.WARNING, "ERROR. No hay saldo suficiente en " + metodoId.getSelectionModel().getSelectedItem());
                     alert.showAndWait();
-
                 }
                 else {
                     while (rs.next()) {
                         inventoryDAO.newRegister(idAbono, rs.getDate("hoy"), rs.getTimestamp("hoy"), tipoMovimiento, monto, cuentaXAbonar.getMonedaTransaccion(), metodoId.getSelectionModel().getSelectedItem(), "ABONO", cuentaXAbonar.getIdTransaction());
                         dbUtils.updateRegister("trans", tipoMetodo, metodoId.getSelectionModel().getSelectedItem(), "id = '" + cuentaXAbonar.getIdTransaction() + "'");
+                        dbUtils.updateRegister("trans",tipoMoneda, moneda, "id = '" + cuentaXAbonar.getIdTransaction() + "'");
                         cuentaXAbonar.actualizarPendiente(ConnectionPool.getConnection(), tipoCuenta);
                     }
     
